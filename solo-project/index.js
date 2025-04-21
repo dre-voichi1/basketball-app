eb443659cf42950c421964772cd2d5901b5dbdf0
dre-voichi1 <cadeleon2024@gmail.com>
2025-04-21 16:20:14 -0400

    An initial commit for the base project
    A new repository built with HTML, CSS, and JavaScript!

---
+++ solo-project/index.js
@@ -0,0 +1,8 @@
+// HTML elements in JavaScript
+let basketBall = document.getElementById("basketball-img");
+
+function showBasketball() {
+  /* Get image and change value of src & alt attribute of basketball image */
+  basketBall.src = "images/basketball.png";
+  basketBall.alt = "A image of a basketball in a large basketball court";
+}
